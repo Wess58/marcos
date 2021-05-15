995b0b65d84aaa523e069c1b5e8ee0979d64aa97
wesley mbate <wmbate@meliora.tech>
2021-05-15 20:02:31 +0300

    added routing for different pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { SolarComponent } from "./solar/solar.component";
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'company',
+    component: AboutComponent
+  },
+  {
+    path: 'solar-solutions',
+    component: SolarComponent
+  },
+  {
+    path: '**', component: HomeComponent
+  },
+  {
+    path: ' ', component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
