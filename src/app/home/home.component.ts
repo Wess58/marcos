69caefd57588a16d372ce7f92788d16589c80455
wesley mbate <wmbate@meliora.tech>
2021-05-26 17:48:44 +0300

    added fadeIn animation on page-entry

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 // import * as AOS from 'aos';
+import { style, state, animate, transition, trigger } from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+  trigger('fadeIn', [
+    transition(':enter', [   // :enter is alias to 'void => *'
+      style({ opacity: 0 }),
+      animate(500, style({ opacity: 1 }))
+    ])
+  ])
+]
 })
 export class HomeComponent implements OnInit {
 
--- src/app/ict-detail/ict-detail.component.ts
+++ src/app/ict-detail/ict-detail.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { style, state, animate, transition, trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-ict-detail',
   templateUrl: './ict-detail.component.html',
-  styleUrls: ['./ict-detail.component.scss']
+  styleUrls: ['./ict-detail.component.scss'],
+  animations: [
+  trigger('fadeIn', [
+    transition(':enter', [   // :enter is alias to 'void => *'
+      style({ opacity: 0 }),
+      animate(500, style({ opacity: 1 }))
+    ])
+  ])
+]
 })
 export class IctDetailComponent implements OnInit {
   tabCurrent: any = 'TELECOMM';
